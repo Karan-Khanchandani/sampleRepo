b95690cf957b45887670b4adad16a9863ed2e6eb
Karan Khanchandani <karan.khanchandani@sap.com>
2017-08-30 15:03:30 +0530

    Adds localhost to listen code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
  
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000,'localhost');
+console.log('Server running at http://localhost:3000/');
  
 module.exports = app;
