305ed7b8184af908ab13a45bc37ef5b851944185
Karan Khanchandani <karan.khanchandani@sap.com>
2017-08-30 22:25:12 +0530

    Revert "Adds localhost to listen code"
    This reverts commit b95690cf957b45887670b4adad16a9863ed2e6eb.
    
    Reverting Comming localhost

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
  
-app.listen(process.env.PORT || 3000,'localhost');
-console.log('Server running at http://localhost:3000/');
+app.listen(process.env.PORT || 3000);
  
 module.exports = app;
